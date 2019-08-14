0043cdbe0cdac4a0ef4decfa86295f4a4e7807b5
J. S. de Boer <j.deboer@spilgames.com>
2019-08-14 13:05:24 +0200

    First unit tests (simple ones) are running

--- test/test_construct_query.py
+++ test/test_construct_query.py
@@ -2,17 +2,18 @@ import os
 import sys
 import json
 import datetime
- 
+
 import unittest
 from unittest import mock
 
-sys.path.append("/opt/container/sample")
+import sample
 from sample.construct_query import construct_query
+
 
 class ConstructQueryTestCase(unittest.TestCase):
     """Test cases for construct_query """
 
-    def test_fields_table(self):
+    def test_fields_table_group(self):
         """Test the normal behaviour of the function"""
 
         fields = ["field1", "field2"]
@@ -32,3 +33,58 @@ class ConstructQueryTestCase(unittest.TestCase):
 
         for field in group:
             assert field in query
+
+    def test_fields_table_nogroup(self):
+        """Test the normal behaviour of the function"""
+
+        fields = ["field1", "field2"]
+        table = "table1"
+
+        query = construct_query(
+            fields=fields,
+            table=table
+        )
+
+        for field in fields:
+            assert field in query
+
+        assert table in query
+
+    def test_fields_table_faulty_field(self):
+        """Test the normal behaviour of the function"""
+
+        fields = True
+        table = "table1"
+        group = ["group1", "group2"]
+        with self.assertRaises(AssertionError):
+            query = construct_query(
+                fields=fields,
+                table=table,
+                group=group
+            )
+
+    def test_fields_table_faulty_table(self):
+        """Test the normal behaviour of the function"""
+
+        fields = ["field1"]
+        table = True
+        group = ["group1", "group2"]
+        with self.assertRaises(AssertionError):
+            query = construct_query(
+                fields=fields,
+                table=table,
+                group=group
+            )
+
+    def test_fields_table_faulty_group(self):
+        """Test the normal behaviour of the function"""
+
+        fields = ["field1"]
+        table = "table1"
+        group = True
+        with self.assertRaises(AssertionError):
+            query = construct_query(
+                fields=fields,
+                table=table,
+                group=group
+            )
