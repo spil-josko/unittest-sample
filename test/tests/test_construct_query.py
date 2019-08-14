7a64bcca3fdaca327b2498c74eebfac7ab5bc993
J. S. de Boer <j.deboer@spilgames.com>
2019-08-14 12:52:52 +0200

    Attempting to import function into test

--- test/tests/test_construct_query.py
+++ test/tests/test_construct_query.py
@@ -6,9 +6,29 @@ import datetime
 import unittest
 from unittest import mock
 
+import container  
+from container import construct_query
 
 class ConstructQueryTestCase(unittest.TestCase):
     """Test cases for construct_query """
 
     def test_fields_table(self):
-        assert True
+        """Test the normal behaviour of the function"""
+
+        fields = ["field1", "field2"]
+        table = "table1"
+        group = ["group1", "group2"]
+
+        query = construct_query(
+            fields=fields,
+            table=table,
+            group=group
+        )
+
+        for field in fields:
+            assert field in query
+
+        assert table in query
+
+        for field in group:
+            assert field in query
